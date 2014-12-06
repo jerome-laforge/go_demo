e1e9d0fd6b4d1e4d925818c6a112ab9508bb1be1
Jerome LAFORGE <jerome.laforge@gmail.com>
2014-12-06 11:35:13 +0100

    Increase number of socket

--- src/S17_goroutine.go
+++ src/S17_goroutine.go
@@ -13,9 +13,9 @@ import (
 
 var wg sync.WaitGroup
 var nbCon, unicPort int32 = 0, 14000
+var nbGoroutine = 10000
 
 func main() {
-	nbGoroutine := 500
 	wg.Add(nbGoroutine)
 	for ; nbGoroutine != 0; nbGoroutine-- {
 		go openSocketReadOrTimeout()
@@ -25,7 +25,7 @@ func main() {
 
 	execThis(fmt.Sprintf("netstat -anpe | grep %d | wc -l", os.Getpid()), true)
 	execThis(fmt.Sprintf("cat /proc/%d/status | grep Threads", os.Getpid()), true)
-	cmd := `echo "Hello World" | nc -uc 127.0.0.1 ` + strconv.Itoa(int(unicPort))
+	cmd := `echo "Hello World" | nc -u 127.0.0.1 ` + strconv.Itoa(int(unicPort))
 	fmt.Println(cmd)
 	time.Sleep(10 * time.Second)
 	execThis(cmd, false)
@@ -44,17 +44,22 @@ func openSocketReadOrTimeout() {
 	if err == nil {
 		defer func() {
 			if con.Close() == nil {
-				fmt.Printf("Connexion est fermée (port = %d). Nombre de connexions restantes ouvertes : %d\n", port, atomic.AddInt32(&nbCon, -1))
+				if _nbCon := atomic.AddInt32(&nbCon, -1); _nbCon%100 == 0 {
+					fmt.Printf("Connexion est fermée (port = %d). Nombre de connexions restantes ouvertes : %d\n", port, _nbCon)
+				}
 			}
 		}()
 
-		fmt.Printf("Connexion est ouverte (port = %d). Nombre de connexions actuellement ouvertes : %d\n", port, atomic.AddInt32(&nbCon, 1))
+		if _nbCon := atomic.AddInt32(&nbCon, 1); _nbCon%100 == 0 {
+			fmt.Printf("Connexion est ouverte (port = %d). Nombre de connexions actuellement ouvertes : %d\n", port, _nbCon)
+		}
 
 		buf := make([]byte, 1500)
 		con.SetDeadline(time.Now().Add(30 * time.Second))
 		n, _, err := con.ReadFromUDP(buf)
 		if err == nil {
-			fmt.Printf("buf[0:%d] = %s", n, buf[0:n])
+			fmt.Printf("(port %d)buf[0:%d] = %s", port, n, buf[0:n])
+			execThis(fmt.Sprintf("netstat -anpe | grep %d | wc -l", os.Getpid()), true)
 		}
 	} else {
 		fmt.Println("Erreur sur l'ouverture de la connexion", err)
